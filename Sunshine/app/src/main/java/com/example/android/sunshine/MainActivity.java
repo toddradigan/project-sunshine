772aa5946bf3dbba53bff51f182a399979c6e7bf
todd radigan <todd.radigan@silvr.ca>
2014-12-02 21:47:52 -0500

    lesson 3: beginning settings work

--- Sunshine/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.sunshine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -36,6 +37,7 @@ public class MainActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
